34181e59777bf1d8f496a780e6b7c884bd6b5af7
greg <chan.gregory.510@gmail.com>
2020-11-12 15:39:02 -0500

    Another fix...

--- build_vim.sh
+++ build_vim.sh
@@ -4,7 +4,7 @@ DISTRO=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
 
 # Install prereqs
 if [[ "$DISTRO" == "ubuntu" ]]; then
-  apt install libncurses5-dev libgtk2.0-dev libatk1.0-dev \
+  sudo apt install libncurses5-dev libgtk2.0-dev libatk1.0-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev \
     python3-dev ruby-dev lua5.2 liblua5.2-dev libperl-dev git
 else
